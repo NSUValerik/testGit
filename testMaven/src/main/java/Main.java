18a9b4b27fcee3c0802e348fdb24ef2acbf2d68a
nsuvalerik <valeraberdov@yandex.ru>
2018-07-14 19:53:54 +0700

    test commin

---
+++ testMaven/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.hibernate.SessionFactory;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SessionFactory sessionFactory;
+        System.out.println("Hello world!!!");
+    }
+}
